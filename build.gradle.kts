5e9c27ecd0345f0e4563007e26d8e238f9caeece
Luis Trigueiros <luistrigueiros@gmail.com>
2018-05-19 12:43:05 +0100

    Configured kotlin support better

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,22 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val kotlinVersion: String by extra
+buildscript {
+    var kotlinVersion: String by extra
+    kotlinVersion = "1.2.41"
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath(kotlin("gradle-plugin", kotlinVersion))
+    }
+}
 plugins {
     java
-    kotlin("jvm") version "1.2.41"
     application
+}
+apply {
+    plugin("kotlin")
 }
 
 application {
@@ -11,9 +26,18 @@ application {
 dependencies {
     compile("com.google.guava:guava:23.0")
     testCompile("junit:junit:4.12")
+    compile(kotlin("stdlib-jdk8", kotlinVersion))
 }
 
 repositories {
     jcenter()
     google()
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
 }
